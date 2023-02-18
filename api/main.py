fcf601338ce12fe34f44cf6d5b4d36f0d525c7bb
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-02-18 05:10:20 -0800

    Fixed indendations

--- api/main.py
+++ api/main.py
@@ -1,16 +1,18 @@
-from flask import Flask, request, redirect, jsonify, render_template, url_for
+from flask import Flask, request, redirect, render_template, url_for
 import db
-app = Flask(__name__, template_folder = "../templates")
+app = Flask(__name__, template_folder='../templates')
 
-@app.route('/', methods = ['GET'])
+
+@app.route('/', methods=['GET'])
 def index():
     return render_template("index.html")
 
-@app.route('/', methods = ['POST'])
+
+@app.route('/', methods=['POST'])
 def get_account():
-    #if the combo doesnt exist 
+    # if the combo doesnt exist
     username = request.get_json()["username"]
-    if(db.user_is_registered(username)):
+    if db.user_is_registered(username):
         return redirect(url_for('web.html'))
     else:
         return redirect(url_for('signup.html'))
@@ -19,6 +21,6 @@ def get_account():
 
 # web pages
 
-@app.route('/add', methods = ['POST'])
+@app.route('/add', methods=['POST'])
 def add_web():
-    # TODO
+    return render_template("add.html")
