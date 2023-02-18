aab290a9e86a524def5f637051c46a8a13909d92
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-02-18 01:10:18 -0800

    Added comments

--- src/test.py
+++ src/test.py
@@ -5,20 +5,20 @@ from dotenv import load_dotenv
 load_dotenv()
 db.initialize()
 
+# Test input
 username = "kyle"
 password = "hunter1234"
 
+# Hash the password
 salt = bcrypt.gensalt()
 hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
-print(hashed_password)
+# Store the hashed password and retrieve it
 db.insert_master_password(username, hashed_password)
+stored_hash = bytes(db.get_master_password(username)[0])
 
-new_pw = bytes(db.get_master_password(username)[0])
-salt = new_pw[:29]
-
-# Verify the entered password against the stored hash and salt values
-if bcrypt.checkpw(password.encode('utf-8'), new_pw):
+# Verify the entered password against the stored hash
+if bcrypt.checkpw(password.encode('utf-8'), stored_hash):
     print("It Matches!")
 else:
     print("Incorrect password")
