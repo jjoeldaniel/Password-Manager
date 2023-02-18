e6fc14abcd79257c19bb6c22d10317fc1619649b
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-02-18 00:07:30 -0800

    Now take user_name as parameters for all functions

--- src/db.py
+++ src/db.py
@@ -1,9 +1,6 @@
 import psycopg2
 import os
-from dotenv import load_dotenv
 import urllib.parse as urlparse
-
-load_dotenv()
 
 
 def connect():
@@ -17,35 +14,44 @@ def connect():
     )
 
 
-def insert_password(password):
+def get_master_password(user_name):
     with connect().cursor as cur:
-        insert_password = f'''
-        INSERT INTO users (passwords) VALUES ({password})
+        get_master_password = '''
+        SELECT master_password FROM users WHERE user_name = %s
         '''
 
-        cur.execute(insert_password, (password,))
-        cur.commit()
+        cur.execute(get_master_password, (user_name,))
+        return cur.fetchone()
 
 
-def insert_master_password(master_password):
+def insert_password(user, hashed_password):
     with connect().cursor as cur:
-        insert_master_password = f'''
-        INSERT INTO users (master_password) VALUES ({master_password})
+        insert_password = '''
+        INSERT INTO users (user_name, passwords) VALUES (%s, %s)
         '''
 
-        cur.execute(insert_master_password, (master_password,))
-        cur.commit()
+        cur.execute(insert_password, (user, hashed_password))
+
+
+def insert_master_password(user_name, master_password):
+    with connect() as conn:
+        with conn.cursor() as cur:
+            insert_master_password = '''
+            INSERT INTO users (user_name, master_password) VALUES (%s, %s)
+            '''
+
+            cur.execute(insert_master_password, (user_name, master_password))
 
 
 def initialize():
-    with connect().cursor as cur:
+    with connect() as conn:
+        with conn.cursor() as cur:
+            initialize_db = '''
+            CREATE TABLE IF NOT EXISTS users(
+                user_name text PRIMARY KEY NOT NULL,
+                master_password text NOT NULL,
+                passwords text[]
+            )
+            '''
 
-        initialize_db = '''
-        CREATE TABLE IF NOT EXISTS users(
-            master_password text PRIMARY KEY,
-            passwords text[]
-        )
-        '''
-
-        cur.execute(initialize_db)
-        cur.commit()
+            cur.execute(initialize_db)
