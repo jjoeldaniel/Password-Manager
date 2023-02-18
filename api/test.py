70318f2e5018635821b1ecb5b81d1cd8d1295e8e
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-02-18 02:40:10 -0800

    Added test case

--- api/test.py
+++ api/test.py
@@ -1,20 +1,30 @@
 import bcrypt
 import db
+import unittest
 from dotenv import load_dotenv
 
-load_dotenv()
-db.initialize()
 
-# Test input
-username = "kyle"
-password = "hunter1234"
+class TestPasswordMethods(unittest.TestCase):
 
-# Store the hashed password and retrieve it
-db.insert_master_password(username, password)
-stored_hash = bytes(db.get_master_password(username)[0])
+    load_dotenv()
+    db.initialize()
 
-# Verify the entered password against the stored hash
-if bcrypt.checkpw(password.encode('utf-8'), stored_hash):
-    print("It Matches!")
-else:
-    print("Incorrect password")
+    # Test input
+    global username
+    username = "kyle"
+
+    global password
+    password = "hunter1234"
+
+    # Store the hashed password and retrieve it
+    db.insert_master_password(username, password)
+    global stored_hash
+    stored_hash = bytes(db.get_master_password(username)[0])
+
+    # Verify the entered password against the stored hash
+    def test_password(self):
+        self.assertTrue(bcrypt.checkpw(password.encode('utf-8'), stored_hash))
+
+
+if __name__ == '__main__':
+    unittest.main()
