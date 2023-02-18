dbeb281783567f2a351b12f4a0687d971d2d18d5
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-02-18 01:06:13 -0800

    Added functional example

--- src/test.py
+++ src/test.py
@@ -1,10 +1,24 @@
 import bcrypt
+import db
+from dotenv import load_dotenv
 
+load_dotenv()
+db.initialize()
+
+username = "kyle"
 password = "hunter1234"
+
 salt = bcrypt.gensalt()
 hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
-if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+print(hashed_password)
+db.insert_master_password(username, hashed_password)
+
+new_pw = bytes(db.get_master_password(username)[0])
+salt = new_pw[:29]
+
+# Verify the entered password against the stored hash and salt values
+if bcrypt.checkpw(password.encode('utf-8'), new_pw):
     print("It Matches!")
 else:
-    print("FUCK")
+    print("Incorrect password")
