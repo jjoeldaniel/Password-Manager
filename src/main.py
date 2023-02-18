e232503bfaa093fd6b9e0457d7692e6801bb4ada
davids700 <dsolano7@csu.fullerton.edu>
2023-02-18 04:46:55 -0800

    Html Progress
    Include a simple HTML file to interact with the backend.

---
+++ src/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify, render_template
+app = Flask(__name__, template_folder = "../templates")
+import db
+
+@app.route('/', methods = ['GET'])
+def index():
+    return render_template("index.html")
+
+@app.route('/', methods = ['POST'])
+def get_account():
+    #if the combo doesnt exist 
+    username = request.get_json()["username"]
+    if(!user_is_registered(username)):
+        return redirect(url_for('signup.html'))
+    else:
+        return redirect(url_for('signup.html'))
+            
+
+        
+       # password = request.get_json()["password"]
+       
+
+
+
+
+        
