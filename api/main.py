65b6fd61917f10293c11bda816cccf40034e2c41
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-02-18 05:08:34 -0800

    Fixed formatting

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, jsonify, render_template
-app = Flask(__name__, template_folder = "../templates")
+from flask import Flask, request, redirect, jsonify, render_template, url_for
 import db
+app = Flask(__name__, template_folder = "../templates")
 
 @app.route('/', methods = ['GET'])
 def index():
@@ -10,23 +10,15 @@ def index():
 def get_account():
     #if the combo doesnt exist 
     username = request.get_json()["username"]
-    if(user_is_registered(username)):
+    if(db.user_is_registered(username)):
         return redirect(url_for('web.html'))
     else:
         return redirect(url_for('signup.html'))
-       # password = request.get_json()["password"]
+        # password = request.get_json()["password"]
 
 
 # web pages
 
 @app.route('/add', methods = ['POST'])
 def add_web():
-    #the 
-    
-
-       
-
-
-
-
-        
+    # TODO
